7387e7f7926f95fc68a7b4247bf81a41da96a2b0
jaehyeok <jaehyeok7633@naver.com>
2024-05-09 13:23:21 +0900

    commit 2

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -3,20 +3,32 @@ import '../scss/TodoInput.scss';
 import { MdAdd } from 'react-icons/md';
 import cn from 'classnames';
 
-const TodoInput = () => {
+const TodoInput = ({ addTodo }) => {
   // 입력창이 열리는 여부를 표현하는 상태값
   const [open, setOpen] = useState(false);
+
+  // 할 일 입력창에 입력한 내용을 표현하는 상태값
+  const [todoText, setTodoText] = useState('');
 
   // + 버튼 클릭 시 이벤트
   const onToggle = () => {
     setOpen(!open);
   };
 
+  // submit 이벤트 핸들러
+  const submitHandler = (e) => {
+    e.preventDefault(); // 태그의 기본 기능 제한(submit 막기)
+    addTodo();
+  };
+
   return (
     <>
       {open && (
         <div className='form-wrapper'>
-          <form className='insert-form'>
+          <form
+            className='insert-form'
+            onSubmit={submitHandler}
+          >
             <input
               type='text'
               placeholder='할 일을 입력 후, 엔터를 누르세요!'
@@ -33,6 +45,7 @@ const TodoInput = () => {
        {클래스이름: 논리값}, 
        클래스 이름 지정 안할 시 변수명이 클래스 이름으로 사용됨.
     */}
+
       <button
         className={cn('insert-btn', { open })}
         onClick={onToggle}
--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoHeader from './TodoHeader';
 import TodoMain from './TodoMain';
 import TodoInput from './TodoInput';
 import '../scss/TodoTemplate.scss';
 
 const TodoTemplate = () => {
+  // 백엔드 서버에 할 일 목록(json)을 요청(fetch)해서 받아와야 함. -> 나중에 하자.
+
+  // todos 배열을 상태 관리
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: '아침 산책하기',
+      done: true,
+    },
+    {
+      id: 2,
+      title: '오늘 주간 신문 읽기',
+      done: true,
+    },
+    {
+      id: 3,
+      title: '샌드위치 사먹기',
+      done: false,
+    },
+    {
+      id: 4,
+      title: '리액트 복습하기',
+      done: false,
+    },
+  ]);
+
+  const addTodo = (todoText) => {
+    console.log('할일:', todoText);
+  };
+
   return (
     <div className='TodoTemplate'>
       <TodoHeader />
       <TodoMain />
-      <TodoInput />
+      <TodoInput addTodo={addTodo} />
     </div>
   );
 };
