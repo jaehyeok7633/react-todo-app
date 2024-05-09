3981e3e7d8ad515cbe134cfd305d41782859a46a
jaehyeok <jaehyeok7633@naver.com>
2024-05-09 12:41:18 +0900

    initial commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import '../scss/todoItem.scss';
+import { MdDelete, MdDone } from 'react-icons/md';
 
 const TodoItem = () => {
-  return <li className='todo-list-item'></li>;
+  return (
+    <li className='todo-list-item'>
+      <div className='check-circle'>
+        <MdDone />
+      </div>
+      <span className='text'>할 일 어쩌고~~~</span>
+      <div className='remove'>
+        <MdDelete />
+      </div>
+    </li>
+  );
 };
 
 export default TodoItem;
