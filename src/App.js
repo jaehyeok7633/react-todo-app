8ecaf8b904e1de67a92e02898c36b454636d487c
jaehyeok <jaehyeok7633@naver.com>
2024-06-13 12:00:28 +0900

    다양한 url에 따른 라우터 설계, useParams(), useSearchParams() 사용법

--- src/App.js
+++ src/App.js
@@ -2,34 +2,23 @@ import './App.css';
 import TodoTemplate from './components/todo/TodoTemplate';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
-import { Route, Routes } from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  Routes,
+} from 'react-router-dom';
 import Login from './components/user/Login';
 import Join from './components/user/Join';
 import { AuthContextProvider } from './utils/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import KakaoLoginHandler from './components/user/KakaoLoginHandler';
+import root from './router/root';
 
 function App() {
   return (
     // 데이터를 전달하고자 하는 자식 컴포넌트를 Provider로 감쌉니다.
     <AuthContextProvider>
-      <div className='wrapper'>
-        <Header />
-
-        <div className='content-wrapper'>
-          <Routes>
-            <Route path='/' element={<TodoTemplate />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/join' element={<Join />} />
-            <Route
-              path='/oauth/kakao'
-              element={<KakaoLoginHandler />}
-            />
-          </Routes>
-        </div>
-
-        <Footer />
-      </div>
+      <RouterProvider router={root} />
     </AuthContextProvider>
   );
 }
